a658a4ef83cb1ec753806a31b8587e7f1dd20f32
dogyeong <shroad1802@naver.com>
2020-07-29 10:58:38 +0900

    Understanding Typescript Core Libs

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ tsc init : 현재 폴더를 타입스크립트 폴더로 초기화한다.
 .tsconfig 파일이 생성되고, tsc 커맨드를 입력하면 폴더 내의 모든 타입스크립트 파일이 컴파일된다.
 tsc -w
 */
+/* 35
+.tsconfig 에 inlude, exclude 옵션을 추가할 수 있다.
+inlclude는 생략하면 폴더 내의 모든 타입스크립트 파일이 대상이 되는 듯 하다.
+include, exclude 옵션이 존재할 경우 include - exclude에 포함되는 파일들이 컴파일된다.
+*/
+// "exclude": ["ts", "node_modules"],
+// "include": ["./*.ts"]
--- app.ts
+++ app.ts
@@ -10,8 +10,27 @@ tsc init : 현재 폴더를 타입스크립트 폴더로 초기화한다.
 tsc -w
 */
 
-/* 35
+/* 36
 .tsconfig 에 inlude, exclude 옵션을 추가할 수 있다.
 inlclude는 생략하면 폴더 내의 모든 타입스크립트 파일이 대상이 되는 듯 하다.
 include, exclude 옵션이 존재할 경우 include - exclude에 포함되는 파일들이 컴파일된다.
+*/
+// "exclude": ["ts", "node_modules"],
+// "include": ["./*.ts"]
+
+/* 37, 38
+.tsconfig 옵션
+target : 컴파일될 자바스크립트의 버전을 지정할 수 있다. es5, es6, es2018 등등..
+
+lib : 사용할 라이브러리를 배열 형태로 정의한다. 만약 lib 항목을 주석처리하고 정의하지 않으면 기본값이 자동으로 정의된다.
+- ES5의 기본 값: dom, es5, scripthost
+- ES6의 기본 값: dom, dom.iterable, es6, scripthost
+
+직접 정의하려면 아래와 같이 배열 형태로 넣어주면 된다
+"lib": [
+  "dom",
+  "dom.iterable",
+  "es6",
+  "scripthost"
+]
 */
