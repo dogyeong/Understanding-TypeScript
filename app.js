9ba97ed4b956e94adbcacf7f4fa266d7ac474e2f
dogyeong <shroad1802@naver.com>
2020-08-01 16:44:35 +0900

    'private' and 'public' Access Modifiers

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 "use strict";
 /* 59
 타입스크립트에서 클래스 사용하기
+public, private 키워드를 필드에 사용할 수 있다.
+하지만 컴파일하고나서, 런타임 때는 적용되지 않는다.
 */
 var Department = /** @class */ (function () {
     function Department(n) {
         this.name = n;
+        this.employees = [];
+        this.phone = 123;
     }
+    Department.prototype.describe = function () {
+        console.log('department: ' + this.name);
+    };
     return Department;
 }());
-new Department('Accounting');
+var accounting = new Department('Accounting');
+accounting.describe();
--- app.ts
+++ app.ts
@@ -1,12 +1,23 @@
 /* 59
 타입스크립트에서 클래스 사용하기
+public, private 키워드를 필드에 사용할 수 있다.
+하지만 컴파일하고나서, 런타임 때는 적용되지 않는다. 
 */
 class Department {
   name: string;
+  private employees: string[]; // private 키워드 사용 가능하다
+  public phone: number; // public 키워드도 사용 가능하다. 생략하면 기본으로 public이 된다.
 
   constructor(n: string) {
     this.name = n;
+    this.employees = [];
+    this.phone = 123;
+  }
+
+  describe(this: Department) {
+    console.log('department: ' + this.name);
   }
 }
 
-new Department('Accounting');
+const accounting = new Department('Accounting');
+accounting.describe();
