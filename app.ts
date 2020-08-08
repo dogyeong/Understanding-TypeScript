15156e51badcd67347afa5bb988b0b5d6ef26c3c
dogyeong <shroad1802@naver.com>
2020-08-08 21:16:22 +0900

    Readonly Interface Properties

--- app.ts
+++ app.ts
@@ -25,6 +25,8 @@ let user: Person = {
 /* 73
 클래스에서 인터페이스를 implements 하여 구현하면
 해당 인터페이스에서 선언된 필드와 메소드를 가져야 한다.
+인터페이스는 클래스나 객체에서 세부 구현은 신경쓰지 않고, 
+특정 필드나 메소드를 가지게 할 때 사용한다.
 */
 interface Greetable {
   greet(): void;
@@ -35,3 +37,17 @@ class Cat implements Greetable {
     console.log('hi');
   }
 }
+
+/* 75
+인터페이스에서 readonly 키워드를 사용할 수 있다.
+readonly 필드는 한번 값이 할당되면, 바꿀 수 없다.
+ */
+interface runnable {
+  readonly speed: number;
+}
+
+let p: runnable = {
+  speed: 12,
+};
+
+// p.speed = 10; // Error
