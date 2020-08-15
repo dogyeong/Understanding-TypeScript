fd4bc048189a027e81afe0bc6a39248b67e93d65
dogyeong <shroad1802@naver.com>
2020-08-15 14:46:49 +0900

    Type Casting

--- app.ts
+++ app.ts
@@ -42,3 +42,28 @@ if (v1 instanceof ExampleClass) {
 if ('go' in v1) {
   v1.go();
 }
+
+/*
+타입 캐스팅
+타입을 임의로 바꿀 수 있다.
+DOM을 다룰 때 특히 유용하게 쓸 수 있다.
+*/
+// input은 id를 이용해서 DOM 객체를 가져왔기 때문에
+// 타입스크립트는 input이 어떤 태그인지 알지 못한다.
+// 그래서 그냥 HTMLElement 타입으로 추론된다.
+const input = document.getElementById('input-tag')!;
+
+// HEMLElement로 추론되었기 때문에 input 태그의 value 프로퍼티를 사용할 수 없다.
+// input.value = 'some text';
+
+// 타입 캐스팅은 2가지 방법이 있다
+// '<타입>' 을 앞에 붙이는 방식
+const input2 = <HTMLInputElement>document.getElementById('input-tag');
+
+// 또는 'as 타입' 을 뒤에 붙이는 방식
+const input3 = document.getElementById('input-tag') as HTMLInputElement;
+
+input2.value = input3.value = 'some text';
+
+// 이렇게도 된다
+(input as HTMLInputElement).value = 'some text';
