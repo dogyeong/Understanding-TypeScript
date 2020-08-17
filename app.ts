c0c61d96affd8ab71c851f02cd4e46fe133192f5
dogyeong <shroad1802@naver.com>
2020-08-17 23:48:48 +0900

    Function Overloads

--- app.ts
+++ app.ts
@@ -94,3 +94,42 @@ const info: nameAndHeight = {
   김철수: 172,
   안철수: 170,
 };
+
+/*
+ 타입스크립트에서는 기본적으로 딱 함수를 선언할 때 명시한 만큼의 인자를 받아야만 타입 에러를 피할 수 있다.
+ 하지만 이렇게 정해진 숫자의 인자만 받을 수 있다면 많은 프로그래밍 요구사항을 해결하기가 어려워 진다.
+ 그래서 한 함수가 여러 쌍의 매개변수-반환 타입 쌍을 갖는 경우를 다룰 수 있게 하고자
+ 타입스크립트는 함수 오버로딩(function overloading)을 지원한다.
+ 타입스크립트의 함수 오버로딩은 다음과 같은 특징을 갖는다.
+ - 함수는 하나 이상의 타입 시그니처를 가질 수 있다.
+ - 함수는 단 하나의 구현을 가질 수 있다.
+*/
+
+// 이 함수들은 각각 문자열, 숫자, 그리고 불리언의 배열을 받아 두 배로 만드는 함수다.
+// 이 때, ‘두 배’가 의미하는 건 타입에 따라 다르다.
+function doubleString(str: string): string {
+  return `${str}${str}`;
+}
+function doubleNumber(num: number): number {
+  return num * 2;
+}
+function doubleBooleanArray(arr: boolean[]): boolean[] {
+  return arr.concat(arr);
+}
+
+// 이 세 함수를 함수 오버로딩을 사용해서 하나의 double 이라는 함수로 합쳐보자.
+function double(str: string): string;
+function double(num: number): number;
+function double(arr: boolean[]): boolean[];
+function double(arg: string | number | boolean[]) {
+  if (typeof arg === 'string') {
+    return `${arg}${arg}`;
+  } else if (typeof arg === 'number') {
+    return arg * 2;
+  } else if (Array.isArray(arg)) {
+    return arg.concat(arg);
+  }
+}
+
+const n = double(2); // number
+const s = double('hi'); // string
