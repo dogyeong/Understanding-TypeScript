5154997e1a9bfe4ff93e1bb3b8873809a9b13c6b
dogyeong <shroad1802@naver.com>
2020-09-26 16:40:59 +0900

    Adding a Production Workflow

---
+++ exercise-01-drag-drop-ESModule-webpack/webpack.config.production.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devtool: 'none',
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+};
