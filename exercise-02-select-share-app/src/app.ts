4d175194ea619c61558625cda7620808c9675e06
dogyeong <shroad1802@naver.com>
2020-12-28 21:54:15 +0900

    Rendering a Map with Google Maps

--- exercise-02-select-share-app/src/app.ts
+++ exercise-02-select-share-app/src/app.ts
@@ -1,9 +1,46 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyDrvPd0707gbvn7kzobGLXu5HfoOecDBfY';
+
+type GoogleGeocodingResponse = {
+  results: {
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      };
+    };
+  }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center: coordinates,
+        zoom: 16,
+      });
+      const marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+    })
+    .catch(alert);
 }
 
 form.addEventListener('submit', searchAddressHandler);
