f3088ddc26057f14a4b635b57ca4ab0e23a2ee1d
dogyeong <shroad1802@naver.com>
2020-12-29 00:50:19 +0900

    Managing State Better

--- exercise-03-react-ts/src/App.tsx
+++ exercise-03-react-ts/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text }]);
+    setTodos((prevTodos) => [...prevTodos, { id: Math.random().toString(), text }]);
   };
 
   return (
